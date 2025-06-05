81513f1359465d598bc4c95c0fee17af74f0a0f5
jay-dhandhukiya <jayddd838@gmail.com>
2025-06-05 16:06:18 +0530

    Update HeroSection.jsx

--- src/components/ui/HeroSection.jsx
+++ src/components/ui/HeroSection.jsx
@@ -1,6 +1,5 @@
 import React from 'react'
 import { FaLongArrowAltRight } from 'react-icons/fa'
-import { NavLink } from "react-router-dom";
 function HeroSection() {
   return (
       <div className="h-full gap-4 py-25 text-white bg-black w-full flex flex-col justify-center items-center sm:flex-col px-[10%]  md:flex-row xl:px-[15%]">
@@ -10,10 +9,8 @@ function HeroSection() {
             Discover the history, culture, and beauty of every nation. Sort,
             search, and filter through countries to find the details you need.
           </p>
-          <button className="flex items-center cursor-pointer gap-2 mt-4 py-2 rounded-full border border-white px-4 bg-gray-900 hover:border-gray-400 md:text-lg">
-            <NavLink to="/country" className="backBtn">
+          <button onClick={() => {window.location.href = '/country'}} className="flex items-center cursor-pointer gap-2 mt-4 py-2 rounded-full border border-white px-4 bg-gray-900 hover:border-gray-400 md:text-lg">
             Start Exploring <FaLongArrowAltRight className='pt-1' />
-            <NavLink />
           </button>
         </div>
         <div className="w-full h-full md:pt-15">
