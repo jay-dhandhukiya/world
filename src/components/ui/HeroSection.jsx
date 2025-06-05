327452bea886c6d5e667418b832e0d250a5914e5
jay-dhandhukiya <jayddd838@gmail.com>
2025-06-05 15:53:22 +0530

    Update HeroSection.jsx

--- src/components/ui/HeroSection.jsx
+++ src/components/ui/HeroSection.jsx
@@ -10,7 +10,9 @@ function HeroSection() {
             search, and filter through countries to find the details you need.
           </p>
           <button className="flex items-center cursor-pointer gap-2 mt-4 py-2 rounded-full border border-white px-4 bg-gray-900 hover:border-gray-400 md:text-lg">
+            <NavLink to="/country" className="backBtn">
             Start Exploring <FaLongArrowAltRight className='pt-1' />
+            <NavLink />
           </button>
         </div>
         <div className="w-full h-full md:pt-15">
